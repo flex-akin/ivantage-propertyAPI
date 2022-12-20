ac391eb117f9a64f21fe87937e8fc8ef34e077c1
flex-akin <akintolafelix2121@gmail.com>
2022-12-20 11:50:31 +0100

    propertyID now used to resolve edit

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -2,6 +2,31 @@ const db = require('../models/index')
 const Property = db.Property
 
 
+// reads in the cloudinary env variable - put this before
+require("dotenv").config();
+// we're aliasing version 2 and referencing with a variable
+const cloudinary = require("cloudinary").v2;
+// cloudinary picks up env and is now configured
+console.log(cloudinary.config().cloud_name);
+
+// Node.js SDK Uploader function returns a Promise
+cloudinary.uploader
+  .upload("./assets/images/breakfast.jpg", {
+    // image is the default resource type if you don't specify
+    resource_type: "image",
+  })
+  .then((result) => {
+    // JSON.stringify will provide a formatted string
+    // 1st param is the value to be output
+    // 2nd param null is a function that can be applied to the output
+    // 3rd param is the number of space characters to use for whitespace in formatting the output
+    console.log("success", JSON.stringify(result, null, 2));
+  })
+  .catch((error) => {
+    console.log("error", JSON.stringify(error, null, 2));
+  });
+
+
 exports.getProperty = async (req, res, next) => {
 try{
     const pageAsNumber = Number.parseInt(req.query.page)
@@ -125,7 +150,7 @@ exports.editProperty = async (req, res, next) => {
         })
         res.status(200).json({
             success: true,
-            message: `the property with property code ${property.propertyCode} have been updated`,
+            message: `the property with property code ${propertyID} have been updated`,
             property
     
             
